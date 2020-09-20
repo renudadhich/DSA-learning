ad9044b6d1c37fa23e0e863c464b317f9e23eaef
H331764 <Renu.Dadhich@Honeywell.com>
2020-09-20 21:54:13 +0530

    swap simplified

---
+++ Array/array-manupulation-problem.js
@@ -0,0 +1,15 @@
+/**
+ * Starting with a 1-indexed array of zeros and a list of operations, 
+ * for each operation add a value to each of the array element between two given indices, inclusive. 
+ * Once all operations have been performed, return the maximum value in the array.
+ * Example : 
+ * n =10;
+ * queries = [[1,5,3],[4,8,7],[6,9,1]];
+ * Queries are interpreted as follows:
+     a b k
+    1 5 3
+    4 8 7
+    6 9 1
+Add the values of  k between the indices a and b  inclusive:
+so aft 
+ */
--- Array/minimum-swaps-arr-sorting.js
+++ Array/minimum-swaps-arr-sorting.js
@@ -1,30 +1,55 @@
-/**
- *
- */
+/**You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] 
+ * without any duplicates. You are allowed to swap any two elements. 
+ * You need to find the minimum number of swaps required to sort the array in ascending order.
+For example, given the array 
+ * arr = [4,3,2,1]
+  output - 3  
+  explaination -> check whether arr[index] is equal to index+1 ..if yes ..move to next item
+   else ==> check till index+1 == arr[index] => index++;
+    first swap index 0 element 4 with it's correct position 3  => 1,3,2,4
+    now check for index 1 and swap 3 with index[2] that is 2 => 1,2,3,4
+    hence array is sorted :)
+*/
 
-const minimumSwaps = (unSortedArr) => {
-  let visited = {},
-    element,
-    swapSum = 0,
-    temp;
-  for (let index = 0; index < unSortedArr.length; index++) {
-    if (!visited[index]) {
-      visited[index] = true;
-      if (index + 1 !== unSortedArr[index]) {
-        element = index;
-        while (unSortedArr[element] != index + 1) {
-          element++;
-        }
-        temp = unSortedArr[element];
-        unSortedArr[element] = unSortedArr[index];
-        unSortedArr[index] = temp;
-        swapSum++;
-      }
+// const minimumSwaps = (unSortedArr) => {
+//   let visited = {},
+//     element,
+//     swapSum = 0,
+//     temp;
+//   for (let index = 0; index < unSortedArr.length; index++) {
+//     if (!visited[index]) {
+//       visited[index] = true;
+//       if (index + 1 !== unSortedArr[index]) {
+//         element = index;
+//         while (unSortedArr[element] != index + 1) {
+//           element++;
+//         }
+//         temp = unSortedArr[element];
+//         unSortedArr[element] = unSortedArr[index];
+//         unSortedArr[index] = temp;
+//         swapSum++;
+//       }
+//     }
+//   }
+//   return swapSum;
+// };
+const swap = (arr, index, target) => {
+  let temp = arr[index];
+  arr[index] = arr[target];
+  arr[target] = temp;
+};
+function minimumSwaps2(arr) {
+  let swapCount = 0;
+  for (let i = 0; i < arr.length; i++) {
+    while (arr[i] !== i + 1) {
+      swap(arr, arr[i] - 1, i);
+      swapCount++;
     }
   }
-  return swapSum;
-};
-
+  console.log("Sorted Array", arr.toString());
+  return swapCount;
+}
 const inputArr = [6, 3, 8, 10, 7, 4, 5, 2, 1, 9];
-console.log('minimum swaps', minimumSwaps(inputArr));
+//console.log('minimum swaps', minimumSwaps(inputArr));
+console.log('minimum swaps 2', minimumSwaps2(inputArr));
 console.log('minimum swaps', minimumSwaps([1, 4, 2, 3]));
