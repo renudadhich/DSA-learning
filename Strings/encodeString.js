20a071f2c9cdeb486a1a2bb711d4f1ec37ba50c4
Renu Dadhich <Renu.Dadhich@walmart.com>
2024-11-07 12:29:32 +0530

    added ecode string program

---
+++ Strings/encodeString.js
@@ -0,0 +1,27 @@
+aabbbcccdd => a2b3c3d2
+/** encode above string into given format */
+
+const encodeStr = (str) => {
+   let prev = str[0];
+   let count = 0;
+   let output = "";
+  
+   for(var i= 0; i < str.length; i++) {
+      if(prev !== str[i]){
+        console.log("count ", count);
+         output += `${prev}${count}`;
+         prev = str[i];
+         count = 1;
+        
+      } else {
+         count++;
+      }
+     if(i === str.length-1) {
+        output += `${prev}${count}`; 
+     }
+
+   }
+   return output;
+}
+
+console.log(encodeStr("aaaabbcccccddd"));
